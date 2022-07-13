557e23550e4a14523436279eb3cea8122be0c70c
luisemorav <luisemorav@hotmail.com>
2022-07-12 23:09:28 -0500

    bootstrap

--- semana7/tasklist/index.js
+++ semana7/tasklist/index.js
@@ -1,29 +1,3 @@
-// document.querySelector
-// jquery
-
-/* $('element')
-Ejemplo
- h1
- $('h1')
- div
- $('div')
-
- id="container"
- $('#container')
-
- class="input-nombre"
- $('.input-nombre')
-*/
-
-//* Nota: Esta funcion que empieza con $(function) se
-//* ejecuta cuando la pagina web inicia
-//* Esto es solo en JQUERY
-
-//* En JS puro IIFE
-// (function () {
-//   // hacemos algo
-// })();
-
 const inputTask = $("#input-task");
 const btnTask = $("#btn-task");
 const sectionTask = $("#section-task");
@@ -37,7 +11,6 @@ $(function () {
   }
 });
 
-// btnTask.on("click", function () {});
 btnTask.click(function () {
   const text = inputTask.val();
 
@@ -47,74 +20,74 @@ btnTask.click(function () {
   inputTask.val("");
   inputTask.focus();
 
-  createInputTask(task.id, task.text);
-
-  // para ocultar un elemento usamos hide()
-  // hide() => para ocultar
-  // show() => para mostrar
-  // inputTask.fadeOut(2000);
-
-  // sectionTask.append(`<h1>${text}</h1>`);
-  // esto es crear un elemento div
+  createInputTask(task.id, task.text, task.status);
 });
 
 function deleteTask(element) {
-  const container = $(element).parent();
-
-  container.children("span").css({
-    "text-decoration": "line-through",
-  });
-
-  container.children("button").hide();
-
-  const id = container.children("input").data("id");
+  const div_task = $(element).closest(".row");
+  const id = div_task.data("id");
+  div_task
+    .find("label")
+    .addClass("text-uppercase text-decoration-line-through");
   updateTask(id, "status", "delete");
 }
 
-function saveTask(element, id) {
-  const container = $(element).parent();
-  const containerFather = $(element).parent().parent();
-  const newText = container.children("input").val();
+function saveTask(element) {
+  const div_task = $(element).closest(".row");
+  const id = Number(div_task.attr("data-id"));
+  const newText = div_task.find("input").val();
   updateTask(id, "text", newText);
-  container.children().hide();
-  containerFather.children().show();
-  containerFather.children("span").text(newText);
+  const task = arrayTask.find((task) => task.id === id);
+  div_task.html(`
+    <div class='col-6 col-sm-8 col-md-9'>
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="check_${id}">
+        <label class="form-check-label ${task.status}" for="check_${id}">
+          ${task.text}
+        </label>
+      </div>
+    </div>
+    <div class='col-6 col-sm-4 col-md-3'>
+      <button class='btn btn-light' onclick="editTask(this)">✏️</button>
+      <button class='btn btn-light'>👁</button>
+      <button class='btn btn-dark' onclick="deleteTask(this)">❌</button>
+    </div>
+  `);
 }
 
 function editTask(element) {
-  const container = $(element).parent();
-  const id = container.children("input").data("id");
-  container.children().hide();
-  container.append(`
-    <div>
-    <input placeholder="Editar tarea"/>
-    <button onclick="saveTask(this, ${id})">✅</button>
-    </div>
-    `);
+  const div_task = $(element).closest(".row");
+  const id = div_task.attr("data-id");
+  div_task.html(`
+      <div class='col-6 col-sm-8 col-md-9'>
+        <input placeholder="editar tarea" type="text" class="form-control"/>
+      </div>
+      <div class='col-6 col-sm-4 col-md-3'>
+        <button class="btn btn-dark" onclick="saveTask(this)">✅</button>
+      </div>
+  `);
 }
 
 function createInputTask(id, text, status) {
-  $("<div>", {
-    html: `
-        <input data-id="${id}" type="checkbox"><span class="${status}">${text}</span>
-        <button onclick="editTask(this)" class="${status}">✏️</button>
-        <button class="${status}">👁</button>
-        <button onclick="deleteTask(this)" class="${status}">❌</button>
-      `,
-  })
+  $("<div class='row my-2' data-id='" + id + "'>")
+    .html(
+      `
+      <div class='col-6 col-sm-8 col-md-9'>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="check_${id}">
+          <label class="form-check-label ${status}" for="check_${id}">
+            ${text}
+          </label>
+        </div>
+      </div>
+      <div class='col-6 col-sm-4 col-md-3'>
+        <button class='btn btn-light' onclick="editTask(this)">✏️</button>
+        <button class='btn btn-light'>👁</button>
+        <button class='btn btn-dark' onclick="deleteTask(this)">❌</button>
+      </div>
+    `
+    )
     .appendTo(sectionTask)
     .hide()
     .fadeIn(1000);
-
-  $("div").hover(
-    // este estilo se va a mantener
-    function () {
-      $(this).css("background-color", "#cdcdcd");
-    },
-    // si ponemos otra funcion separa por , entendera que se ejecutara
-    // cuando saque el mouse el element
-    function () {
-      $(this).css("background-color", "#fff");
-    }
-  );
 }
