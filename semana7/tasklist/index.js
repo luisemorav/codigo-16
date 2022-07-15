648cabb17ffac953e7ea814cd08fd98128443761
luisemorav <luisemorav@hotmail.com>
2022-07-14 22:11:02 -0500

    noty y filters

--- semana7/tasklist/index.js
+++ semana7/tasklist/index.js
@@ -1,93 +1,160 @@
 const inputTask = $("#input-task");
 const btnTask = $("#btn-task");
+const formTask = $("#form-task");
 const sectionTask = $("#section-task");
+const sectionDetailTask = $("#section-detail-task");
+const form = document.querySelector("#form-task");
 
 $(function () {
+  // vamos a obtener el queryString de la url
+  const queryString = new URLSearchParams(window.location.search);
+  const filter = queryString.get("filter"); // todo || done || delete
+
   if (arrayTask.length > 0) {
-    // aca iteremos el array y pintemos las tareas
     arrayTask.forEach((task) => {
-      createInputTask(task.id, task.text, task.status);
+      if (filter) {
+        if (task.status == filter || filter == "all") {
+          addTaskToSection(task);
+        }
+      } else {
+        addTaskToSection(task);
+      }
     });
+
+    if (filter) {
+      switch (filter) {
+        case "todo":
+          $("#filter_todo").addClass("active");
+          break;
+        case "done":
+          $("#filter_done").addClass("active");
+          break;
+        case "done":
+          $("#filter_delete").addClass("active");
+          break;
+        default:
+          $("#filter_all").addClass("active");
+          break;
+      }
+    }
   }
 });
 
-btnTask.click(function () {
-  const text = inputTask.val();
+function showFilteredModalTask(filter) {
+  const filterTask = arrayTask.filter((task) => task.status === filter);
 
+  const tbody = $("#tbody");
+
+  filterTask.forEach((task) => {
+    tbody.append(
+      `<tr>
+            <td>${task.id}</td>
+            <td>${task.text}</td>
+            <td>${task.status}</td>
+            <td>${task.created_at}</td>
+          </tr>
+          `
+    );
+  });
+
+  const myModal = new bootstrap.Modal(document.getElementById("exampleModal"));
+
+  myModal.show();
+}
+
+formTask.submit(function (e) {
+  e.preventDefault();
+  inputTask.focus();
+  //! checkValidity analiza los inputs y si tienen required y estan vacios
+  //! esta funcion retorna false caso contrario esten lleno
+  //* retorna true
+  if (!form.checkValidity()) {
+    // agregamos la clase was-validated al formulario
+    // esto al ser agregado buscara los invalid-feedback y los mostrara
+    form.classList.add("was-validated");
+    return;
+  }
+
+  const text = inputTask.val();
   const task = storeTask(text);
-  console.log("Task", task);
+  // console.log("Task", task);
 
   inputTask.val("");
-  inputTask.focus();
 
-  createInputTask(task.id, task.text, task.status);
+  addTaskToSection(task);
+  new Noty({
+    theme: "relax",
+    type: "success",
+    layout: "topCenter",
+    text: "Nueva tarea agregada!",
+    timeout: 3000,
+  }).show();
 });
 
 function deleteTask(element) {
   const div_task = $(element).closest(".row");
   const id = div_task.data("id");
-  div_task
-    .find("label")
-    .addClass("text-uppercase text-decoration-line-through");
-  updateTask(id, "status", "delete");
+  const task = updateTask(id, "status", "delete");
+  div_task.replaceWith(task.toHtml());
 }
 
 function saveTask(element) {
   const div_task = $(element).closest(".row");
   const id = Number(div_task.attr("data-id"));
   const newText = div_task.find("input").val();
-  updateTask(id, "text", newText);
-  const task = arrayTask.find((task) => task.id === id);
-  div_task.html(`
-    <div class='col-6 col-sm-8 col-md-9'>
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" id="check_${id}">
-        <label class="form-check-label ${task.status}" for="check_${id}">
-          ${task.text}
-        </label>
-      </div>
-    </div>
-    <div class='col-6 col-sm-4 col-md-3'>
-      <button class='btn btn-light' onclick="editTask(this)">✏️</button>
-      <button class='btn btn-light'>👁</button>
-      <button class='btn btn-dark' onclick="deleteTask(this)">❌</button>
-    </div>
-  `);
+  const task = updateTask(id, "text", newText);
+  div_task.replaceWith(task.toHtml());
 }
 
 function editTask(element) {
   const div_task = $(element).closest(".row");
-  const id = div_task.attr("data-id");
   div_task.html(`
       <div class='col-6 col-sm-8 col-md-9'>
         <input placeholder="editar tarea" type="text" class="form-control"/>
       </div>
       <div class='col-6 col-sm-4 col-md-3'>
         <button class="btn btn-dark" onclick="saveTask(this)">✅</button>
+        <button class="btn btn-dark" onclick="resetTask(this)">↩️</button>
       </div>
   `);
 }
 
-function createInputTask(id, text, status) {
-  $("<div class='row my-2' data-id='" + id + "'>")
-    .html(
-      `
-      <div class='col-6 col-sm-8 col-md-9'>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" id="check_${id}">
-          <label class="form-check-label ${status}" for="check_${id}">
-            ${text}
-          </label>
+function resetTask(element) {
+  const div_task = $(element).closest(".row");
+  const id = div_task.data("id");
+  const task = arrayTask.find((task) => task.id === id);
+  div_task.replaceWith(task.toHtml());
+}
+
+function showTask(id) {
+  const task = arrayTask.find((task) => task.id === id);
+  sectionDetailTask.empty().show();
+
+  sectionDetailTask.append(
+    `
+    <div class="card" style="width: 300px; padding: 0;">
+      <img src="https://source.unsplash.com/user/erondu/300x200?v=${Math.random()}" class="card-img-top" />
+      <div class="card-body">
+        <div class="card-title">${task.text}</div>
+        <div class="card-text">
+        ${task.status}
+        </div>
+         <div class="card-text">
+        ${task.created_at}
         </div>
       </div>
-      <div class='col-6 col-sm-4 col-md-3'>
-        <button class='btn btn-light' onclick="editTask(this)">✏️</button>
-        <button class='btn btn-light'>👁</button>
-        <button class='btn btn-dark' onclick="deleteTask(this)">❌</button>
-      </div>
+    </div>
     `
-    )
-    .appendTo(sectionTask)
-    .hide()
-    .fadeIn(1000);
+  );
+}
+
+function doneTask(element, id) {
+  const task = updateTask(id, "status", "done");
+  const div_task = $(element).closest(".row");
+  div_task.replaceWith(task.toHtml());
+}
+
+function addTaskToSection(task) {
+  const div_task = task.toHtml();
+  div_task.appendTo(sectionTask).hide().fadeIn(1000);
 }
--- semana7/tasklist/task.js
+++ semana7/tasklist/task.js
@@ -1,19 +1,72 @@
 let arrayTask = JSON.parse(localStorage.getItem("tasks")) || [];
 
-console.log("arrayTask", arrayTask);
+// Aquí felizmente vamos a recrear la function toHtml() para cada task:
+// arrayTask solo tiene task con prop (NO TIENEN FUNCIONES).
+arrayTask = arrayTask.map((task) => {
+  return {
+    id: task.id,
+    text: task.text,
+    status: task.status,
+    created_at: new Date(task.created_at),
+    toHtml: function () {
+      return taskToHtml(this);
+    },
+  };
+});
+// arrayTask ahora tiene task con PROP + FUNC
 
 function storeTask(text) {
-  // 1,2,3,4,5,6
   const task = {
     id: arrayTask.length + 1,
     text,
     status: "todo",
     created_at: new Date(),
+    toHtml: function () {
+      return taskToHtml(this);
+    },
   };
 
   arrayTask.push(task);
   saveInLocalStorage();
   return task;
+}
+
+function taskToHtml(task) {
+  const div_task = $("<div>", {
+    class: "row my-2 py-2",
+    "data-id": task.id,
+  }).html(`
+  <div class='col-6 col-sm-8 col-md-9'>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" onchange="doneTask(this, ${task.id})" id="check_${task.id}">
+      <label class="form-check-label" for="check_${task.id}">
+        ${task.text}
+      </label>
+    </div>
+  </div>
+  <div class='col-6 col-sm-4 col-md-3'>
+    <button class='btn btn-light' role="edit" onclick="editTask(this)">✏️</button>
+    <button class='btn btn-light' role="show" onclick="showTask(${task.id})">👁</button>
+    <button class='btn btn-dark' role="delete" onclick="deleteTask(this)">❌</button>
+  </div>
+  `);
+  if (task.status == "done") {
+    div_task
+      .find("input[type='checkbox'")
+      .prop("disabled", true)
+      .prop("checked", true);
+    div_task.addClass("bg-info bg-opacity-75 rounded text-white fst-italic");
+    div_task.find("button").each(function () {
+      if ($(this).attr("role") == "show") return;
+      $(this).hide();
+    });
+  } else if (task.status == "delete") {
+    div_task.find("input[type='checkbox'").prop("disabled", true);
+    div_task.addClass("bg-danger bg-opacity-50 rounded text-white");
+    div_task.find("label").addClass("text-decoration-line-through");
+    div_task.find("button").hide();
+  }
+  return div_task;
 }
 
 function updateTask(id, key, value) {
@@ -22,8 +75,10 @@ function updateTask(id, key, value) {
   task[key] = value;
   // para actualizar la data en localStorage solo hace falta llamar a la funcion saveInLocalStorga()
   saveInLocalStorage();
+  return task;
 }
 
+// Aquí lamentablemente se perderán las funciones del objeto task:
 function saveInLocalStorage() {
   localStorage.setItem("tasks", JSON.stringify(arrayTask));
 }
